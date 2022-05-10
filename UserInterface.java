345cd0e13cb0a68c0292375b971ee76e237cdb15
ewu95 <99373638+ewu95@users.noreply.github.com>
2022-05-10 13:02:37 -0400

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-	static Scanner sc = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 		boolean quit = false, badData = true,loggedIn = false;;
 		int input = -1, idx = 1; 
