22282b2037d21243cce7fe82ac9e20943f42c985
ewu95 <99373638+ewu95@users.noreply.github.com>
2022-05-15 19:11:35 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,16 +1,16 @@
 public class Game {
-	/* Attributes */
+    /* Attributes */
 
-	/** list of players in the game */ 
-	private User[] players; 
+    /** list of players in the game */ 
+    private User[] players; 
 
-	/** players' bets on the game */
-	private double[] bets;
+    /** players' bets on the game */ 
+    private double[] bets;
 
-	/** identification number of the game */ 
-	private int gameID; 
+    /** identification number of the game */ 
+    private int gameID; 
 
-	/*
+    /*
     Constructors 
     */
 
@@ -27,63 +27,64 @@ public class Game {
 		
     }
 
-	//Accessors
+    //Accessors
 
-	/**
-	Gets the list of players' usernames as a string
-	@return the players' usernames, space separated 
-	*/ 
-	public User[] getPlayers() {
-		return this.players;
-	}
+    /**
+    Gets the list of players' usernames as a string
+    @return the players' usernames, space separated 
+    */ 
 
-	/**
-	Gets the list of bets as a string
-	@return the players' bets, space separated
-	*/
-	public double[] getBets() {
-		return this.bets;
-	}
+    public User[] getPlayers() {
+	return this.players;
+    }
 
-	/**
-	Gets the game id
-	@return the game id 
-	*/
-	public int getGameID() {
-		return this.gameID; 	
-	}
+    /**
+    Gets the list of bets as a string
+    @return the players' bets, space separated
+    */
+    public double[] getBets() {
+	return this.bets;
+    }
 
-	//Mutators
+    /**
+    Gets the game id
+    @return the game id 
+    */
+    public int getGameID() {
+        return this.gameID; 	
+    }
+
+    //Mutators
 	
-	/**
-	Updates a certain players' bet amount
-	@param username the username of the players' bet to be updated
-	@param val the value that the bet is updated to
-	*/ 
-	public void updateBet(String username, int val) 	{
-		for (int i = 0; i < this.players.length; i++) {
-			if ((this.players[i].getUsername()).equals(username)) {
-				this.bets[i] = val;
-			}
+    /**
+    Updates a certain players' bet amount
+    @param username the username of the players' bet to be updated
+    @param val the value that the bet is updated to
+    */ 
+    public void updateBet(String username, int val) 	{
+	for (int i = 0; i < this.players.length; i++) {
+		if ((this.players[i].getUsername()).equals(username)) {
+			this.bets[i] = val;
 		}
 	}
+    }
 
-	/**
-	Takes the players bets from their wallets
-	*/
-	public void takeMoney() {
-		for (int i = 0; i < this.players.length; i++) {
-			Wallet wallet = this.players[i].getWallet();
-			wallet.changeMoney(-this.bets[i]);
-		}
+    /**
+    Takes the players bets from their wallets
+    */
+    public void takeMoney() {
+	for (int i = 0; i < this.players.length; i++) {
+		Wallet wallet = this.players[i].getWallet();
+		wallet.changeMoney(-this.bets[i]);
 	}
-	/**
-	Returns the players winning to their respective wallets
-	*/ 
-	public void returnMoney() {
-		for (int i = 0; i < this.players.length; i++) {
-			Wallet wallet = this.players[i].getWallet();
-			wallet.changeMoney(this.bets[i]);
-		}
+    }
+    /**
+    Returns the players winning to their respective wallets
+    */ 
+    public void returnMoney() {
+	for (int i = 0; i < this.players.length; i++) {
+		Wallet wallet = this.players[i].getWallet();
+		wallet.changeMoney(this.bets[i]);
 	}
+    }
 }
